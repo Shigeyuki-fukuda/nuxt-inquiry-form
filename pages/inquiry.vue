8abfa33b7eba1cc5026ab63a5c1ab3a55317c2b3
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-04 17:06:23 +0900

    feat: バリデーションを除く、フォームの送信処理と送信後の表示切り替え部分を実装

--- pages/inquiry.vue
+++ pages/inquiry.vue
@@ -9,7 +9,18 @@
             お問い合わせ
           </h1>
         </div>
-        <div class="m-7">
+        <div v-if="isSubmited" class="m-7 text-center">
+          <h1>お問い合わせいただきありがとうございます。</h1>
+          <h2>ご返信までしばらくお待ちください。</h2>
+          <nuxt-link
+            to="/"
+            type="button"
+            class="py-4 px-8 mt-8 text-lg text-gray-50 bg-green-500 hover:bg-green-600 focus:ring-green-600 focus:ring-offset-green-600 text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+          >
+            TOPへ戻る
+          </nuxt-link>
+        </div>
+        <div v-if="!isSubmited" class="m-7">
           <form>
             <div class="mb-6">
               <label
@@ -21,6 +32,7 @@
               </label>
               <input
                 id="name"
+                v-model="name"
                 type="text"
                 name="name"
                 placeholder="お名前太郎"
@@ -37,6 +49,7 @@
               </label>
               <input
                 id="email"
+                v-model="email"
                 type="email"
                 name="email"
                 placeholder="your@example.com"
@@ -52,6 +65,7 @@
               </label>
               <input
                 id="phone"
+                v-model="phone"
                 type="text"
                 name="phone"
                 placeholder="0312345678"
@@ -68,6 +82,7 @@
               </label>
               <textarea
                 id="message"
+                v-model="message"
                 rows="5"
                 name="message"
                 placeholder="お問い合わせ内容です"
@@ -77,8 +92,8 @@
             </div>
             <div class="mb-6">
               <button
-                type="submit"
-                class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                class="w-full px-3 py-4 font-bold text-white bg-green-500 rounded-md focus:bg-green-600 focus:outline-none"
+                @click.prevent="onSubmit"
               >
                 送信する
               </button>
@@ -92,10 +107,38 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, useContext, ref } from '@nuxtjs/composition-api'
 export default defineComponent({
   setup() {
-    return {}
+    const { $axios } = useContext()
+    const name = ref('')
+    const email = ref('')
+    const phone = ref('')
+    const message = ref('')
+    const isSubmited = ref(false)
+
+    const onSubmit = async () => {
+      return await $axios
+        .post('/inquiry', {
+          inquiry: {
+            name,
+            email,
+            phone,
+            message,
+          },
+        })
+        .then(() => (isSubmited.value = true))
+        .catch(() => {})
+    }
+
+    return {
+      name,
+      email,
+      phone,
+      message,
+      isSubmited,
+      onSubmit,
+    }
   },
 })
 </script>
