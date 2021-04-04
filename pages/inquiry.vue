b674c1f882c6cc4e5d72f9cc4f45e5d447434aad
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-04 13:51:54 +0900

    feat: @nuxtjs/composition-apiをscriptタグ内で利用するようにした

--- pages/inquiry.vue
+++ pages/inquiry.vue
@@ -90,3 +90,12 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
