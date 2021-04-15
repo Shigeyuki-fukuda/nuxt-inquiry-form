0cfd39a91a0edf1c42b6d90cd359dfd8b46fdb93
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-15 21:40:15 +0900

    remove: 重複していたユーティリティクラスの削除

--- pages/inquiry.vue
+++ pages/inquiry.vue
@@ -15,7 +15,7 @@
           <nuxt-link
             to="/"
             type="button"
-            class="py-4 px-8 mt-8 text-lg text-gray-50 bg-green-500 hover:bg-green-600 focus:ring-green-600 focus:ring-offset-green-600 text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+            class="py-4 px-8 mt-8 text-lg text-gray-50 bg-green-500 hover:bg-green-600 focus:ring-green-600 focus:ring-offset-green-600 transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
           >
             TOPへ戻る
           </nuxt-link>
