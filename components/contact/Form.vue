b14a113cee3ec889fc132573f4669d13069e5d69
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-17 16:28:36 +0900

    feat: お問い合わせフォームの各項目のラベルをコンポーネントに分割

--- components/contact/Form.vue
+++ components/contact/Form.vue
@@ -6,75 +6,50 @@
         <Submitted v-if="isSubmited" />
         <div v-if="!isSubmited" class="m-7">
           <form>
-            <div class="mb-6">
-              <label
-                for="name"
-                class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-              >
-                お名前
-                <span class="text-xs text-red-500">(必須)</span>
-              </label>
+            <div class="mb-12">
+              <Label>
+                お名前 <span class="text-xs text-red-500">(必須)</span>
+              </Label>
               <input
-                id="name"
                 v-model="name"
                 type="text"
-                name="name"
                 placeholder="お名前太郎"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
             </div>
-            <div class="mb-6">
-              <label
-                for="email"
-                class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-              >
-                メールアドレス
-                <span class="text-xs text-red-500">(必須)</span>
-              </label>
+            <div class="mb-12">
+              <Label>
+                メールアドレス <span class="text-xs text-red-500">(必須)</span>
+              </Label>
               <input
-                id="email"
                 v-model="email"
                 type="email"
-                name="email"
                 placeholder="your@example.com"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
             </div>
-            <div class="mb-6">
-              <label
-                for="phone"
-                class="text-sm text-gray-600 dark:text-gray-400"
-              >
-                電話番号
-              </label>
+            <div class="mb-12">
+              <Label> 電話番号 </Label>
               <input
-                id="phone"
                 v-model="phone"
                 type="text"
-                name="phone"
                 placeholder="0312345678"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
             </div>
-            <div class="mb-6">
-              <label
-                for="message"
-                class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-              >
-                内容
-                <span class="text-xs text-red-500">(必須)</span>
-              </label>
+            <div class="mb-12">
+              <Label>
+                内容 <span class="text-xs text-red-500">(必須)</span>
+              </Label>
               <textarea
-                id="message"
                 v-model="message"
                 rows="5"
-                name="message"
                 placeholder="お問い合わせ内容です"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               >
               </textarea>
             </div>
-            <div class="mb-6">
+            <div class="mb-12">
               <button
                 class="w-full px-3 py-4 font-bold text-white bg-green-500 rounded-md focus:bg-green-600 focus:outline-none"
                 @click.prevent="onSubmit"
@@ -93,12 +68,14 @@
 import { defineComponent, useContext, ref } from '@nuxtjs/composition-api'
 import Headline from '@/components/contact/Headline.vue'
 import Submitted from '@/components/contact/Submitted.vue'
+import Label from '@/components/contact/Label.vue'
 
 export default defineComponent({
   name: 'Form',
   components: {
     Headline,
-    Submitted
+    Submitted,
+    Label,
   },
   setup() {
     const { $axios } = useContext()
